cc6bfbbd29394f602aab75695d0eb40a5e4682db
mohammad-ILA <mghazim93@gmail.com>
2024-04-11 10:18:41 +0330

    add meta description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="fa" dir="rtl">
       <Head>
+        <meta
+          name="description"
+          content="اسنپ فود (زودفود قدیم) سامانه سفارش آنلاین غذا، شیرینی و خرید آنلاین از کافی شاپ و سوپرمارکت ها در تهران، کرج، شیراز، اصفهان، مشهد و سراسر ایران"
+          data-react-helmet="true"
+        />
         <link rel="stylesheet" href="/fonts/font.css" />
       </Head>
       <body>
