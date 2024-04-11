771b3d40ce84e07b1c47a9c08a2157c1c0e7ffb8
mohammad-ILA <mghazim93@gmail.com>
2024-04-11 16:27:41 +0330

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "storybook-addon-rtl",
+  ],
+  staticDirs: [
+    "../public",
+    {
+      from: "../src/components/common/fonts",
+      to: "src/components/common/fonts",
+    },
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
