e1aac7419d823e2cec55df1689da602507f502cf
mohammad-ILA <mghazim93@gmail.com>
2024-04-11 11:35:41 +0330

    refactor card component

--- src/components/common/card/index.tsx
+++ src/components/common/card/index.tsx
@@ -5,6 +5,7 @@ import variables from "@/components/common/styles/variables.module.sass";
 import StarIcon from "../icons/star";
 import { convertEnNumToFa } from "../functions/convert-en-num-to-fa";
 import Image from "@/components/common/image";
+
 const Card: React.FC<CardProps> = ({
   backgroundImage,
   best_coupon,
@@ -18,6 +19,57 @@ const Card: React.FC<CardProps> = ({
   voteCount,
   description,
 }) => {
+  const renderProIcon = () => {
+    if (is_pro) {
+      return (
+        <div className={styles["card__logo-pro-image"]}>
+          <ProIcon width="28" height="28" color={variables.primaryColor} />
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const renderBestCoupon = () => {
+    if (best_coupon) {
+      const convertedBestCouponEnNumbersToFa = convertEnNumToFa(best_coupon);
+      return (
+        <div className={styles.card__offer}>
+          <div className={styles["card__offer-icon"]}>
+            <ProIcon width="21" height="21" color={variables.primaryColor} />
+          </div>
+          <div className={styles["card__offer-text"]}>
+            {convertedBestCouponEnNumbersToFa}
+          </div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const renderDiscount = () => {
+    if (discountValueForView !== undefined && discountValueForView > 0) {
+      return (
+        <div className={styles["card__title-discount"]}>تا {discount}٪</div>
+      );
+    }
+  };
+
+  const deliveryFe =
+    deliveryFee === 0
+      ? "رایگان"
+      : convertEnNumToFa(deliveryFee.toLocaleString()) + " تومان";
+
+  const deliveryType = isZFExpress ? "ارسال اکسپرس" : "پیک فروشنده";
+
+  const rateScore = convertEnNumToFa(rate.toString());
+
+  const rateCount = convertEnNumToFa(voteCount.toLocaleString());
+
+  const discount = convertEnNumToFa(discountValueForView + "");
+
+  const activeClassLogo = is_pro ? styles.card__logo_active : "";
+
   return (
     <section className={styles.card}>
       <header className={styles.card__header}>
@@ -30,11 +82,7 @@ const Card: React.FC<CardProps> = ({
             alt=""
           />
         </div>
-        <div
-          className={
-            styles.card__logo + " " + (is_pro ? styles.card__logo_active : "")
-          }
-        >
+        <div className={`${styles.card__logo} ${activeClassLogo}`}>
           <Image
             loader={({ src }) => src}
             width={48}
@@ -43,45 +91,24 @@ const Card: React.FC<CardProps> = ({
             alt=""
             className={styles["card__logo-image"]}
           />
-          {is_pro && (
-            <div className={styles["card__logo-pro-image"]}>
-              <ProIcon width="28" height="28" color={variables.primaryColor} />
-            </div>
-          )}
+          {renderProIcon()}
         </div>
         <div className={styles.card__advertisement}>
           <div className={styles["card__advertisement-text"]}></div>
           <div className={styles["card__advertisement-icon"]}></div>
         </div>
-        {best_coupon && (
-          <div className={styles.card__offer}>
-            <div className={styles["card__offer-icon"]}>
-              <ProIcon width="21" height="21" color={variables.primaryColor} />
-            </div>
-            <div className={styles["card__offer-text"]}>
-              {convertEnNumToFa(best_coupon)}
-            </div>
-          </div>
-        )}
+        {renderBestCoupon()}
       </header>
       <div className={styles.card__body}>
         <div className={styles["card__title-rate"]}>
           <div className={styles.card__title}>
             <h3 className={styles["card__title-text"]}>{title}</h3>
-            {discountValueForView !== undefined && discountValueForView > 0 && (
-              <div className={styles["card__title-discount"]}>
-                تا {convertEnNumToFa(discountValueForView + "")}٪
-              </div>
-            )}
+            {renderDiscount()}
           </div>
           <div className={styles.card__rate}>
-            <div className={styles["card__rate-count"]}>
-              ({convertEnNumToFa(voteCount.toLocaleString())})
-            </div>
+            <div className={styles["card__rate-count"]}>({rateCount})</div>
             <div className={styles["card__rate-score"]}>
-              <div className={styles["card__rate-score-text"]}>
-                {convertEnNumToFa(rate.toString())}
-              </div>
+              <div className={styles["card__rate-score-text"]}>{rateScore}</div>
               <div className={styles["card__rate-score-icon"]}>
                 <StarIcon
                   width="14"
@@ -95,19 +122,8 @@ const Card: React.FC<CardProps> = ({
         <div className={styles.card__description}>{description}</div>
         <div className={styles["card__send-duration"]}>
           <div className={styles.card__send}>
-            <div className={styles["card__send-method"]}>
-              {isZFExpress ? "ارسال اکسپرس" : "پیک فروشنده"}
-            </div>
-            <div className={styles["card__send-price"]}>
-              {deliveryFee === 0
-                ? "رایگان"
-                : convertEnNumToFa(deliveryFee.toLocaleString()) +
-                  " تومان"}{" "}
-            </div>
-          </div>
-          <div className={styles.card__duration}>
-            <div className={styles["card__duration-text"]}></div>
-            <div className={styles["card__duration-icon"]}></div>
+            <div className={styles["card__send-method"]}>{deliveryType}</div>
+            <div className={styles["card__send-price"]}>{deliveryFe}</div>
           </div>
         </div>
       </div>
